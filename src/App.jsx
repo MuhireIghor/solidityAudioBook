cae1a0fd5be4f872323f6fe990e40f5d88d8a7d7
MuhireIghor <muhireighor123@gmail.com>
2022-12-04 00:16:11 +0200

    [updates]:sidebar finilisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
 import './App.css'
 import Home from './pages/Home'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 function App() {
   return(
 <div>
-  <Home />
+  <Router>
+    <Routes>
+   <Route path='/'exact element={<Home />} /> 
+   <Route path='DashBoard' exact element={<Dashboard />} /> 
+    </Routes>
+  </Router>
     </div>
 
   )
--- src/Data/links.jsx
+++ src/Data/links.jsx
@@ -1,20 +1,27 @@
 import {FiHome} from 'react-icons/fi';
 import {GoSearch} from 'react-icons/go'
 import {BiLibrary} from 'react-icons/bi';
+import {AiFillHome} from 'react-icons/ai';
+import {FaSearch} from 'react-icons/fa';
+import {VscLibrary} from 'react-icons/vsc'
 export const links = [
     {
         name:"Home",
-        redirectUrl:"/Home",
-        icon:<FiHome/>
+        redirectUrl:"Home",
+        normalIcon:<FiHome/>,
+        activeIcon:<AiFillHome/>
     },
     {
         name:"Search",
-        redirectUrl:"/Search",
-        icon:<GoSearch/>
+        redirectUrl:"Search",
+       normalIcon:<GoSearch/>,
+       activeIcon:<FaSearch/>
     },
     {
         name:"Your Library",
-        redirectUrl:"/Library",
-        icon:<BiLibrary />
+        redirectUrl:"Your Library",
+        normalIcon:<VscLibrary/>,
+        activeIcon:<BiLibrary />
+        
     },
 ]
