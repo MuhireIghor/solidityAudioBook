127a8ac23ee3550e15104d597104f2cb75d6b5f1
MuhireIghor <muhireighor123@gmail.com>
2022-12-03 23:42:46 +0200

    Dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const Dashboard = () => {
+  return (
+    <div className='flex'>
+      <div className='basis-2/12'>
+      <Sidebar />
+      </div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
